179abaf21507ea2bec4082dac2aa09efdac9db17
Le Van Pon <spixai2020@gmail.com>
2021-11-19 10:22:07 +0700

    update

--- components/layouts/modules/Head.jsx
+++ components/layouts/modules/Head.jsx
@@ -3,22 +3,23 @@ import Head from 'next/head';
 
 const StyleSheets = () => (
 	<Head>
-		<title>Elsa</title>
+		<title>The world’s best way to improve your English pronunciation | elsaspeak</title>
 		<link rel='shortcut icon' href='/Favicon.png' />
 		<link rel='icon' href='/Favicon.png' sizes='32x32' />
 		<link rel='icon' href='/Favicon.png' sizes='192x192' />
 		<link rel='apple-touch-icon-precomposed' href='/Favicon.png' />
 		<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-		<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+		<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
 		<meta name='format-detection' content='telephone=no' />
 		<meta name='apple-mobile-web-app-capable' content='yes' />
-		<meta name='author' content='Elsa' />
-		<meta name='keywords' content='Elsa' />
-		<meta name='description' content='Elsa' />
+		<meta name='author' content='ELSA Corporation' />
+		<meta name='keywords' content='Learn English, improve pronunciation, ELSA speak, mobile, speak English, pronunciation, accent reduction, Elsa, Vietnam, soccer, football, World Cup' />
+		<meta name='description' content='Our speech recognition app is designed to evaluate and give feedback on your English pronunciation and fluency. With ELSA you can speak English like a natural.' />
+		<meta name="google-site-verification" content="NVLqfTxnXh5Q-F2WGtNz3J_HiAvvdZaiDhk9bRfA-rM"/>
+		<meta charset="utf-8"/>
+
 		<link rel='stylesheet' href='/static/fonts/Linearicons/Font/demo-files/demo.css' />
-
 		<link rel='stylesheet' href='/static/fonts/font-awesome/css/font-awesome.min.css' />
-
 		<link rel='stylesheet' type='text/css' href='/static/css/bootstrap.min.css' />
 		<link rel='stylesheet' type='text/css' href='/static/css/slick.min.css' />
 
